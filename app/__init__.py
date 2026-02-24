c0dbca9cc596fb26b6d27df701c99dd6dc57fc1e
erbreuer <erik.breuer03@gmail.com>
2026-02-24 10:48:01 +0100

    init

--- app/__init__.py
+++ app/__init__.py
@@ -8,20 +8,21 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
-    # CORS aktivieren 
+
+    # CORS aktivieren
     CORS(app)
-    
+
     # Konfiguration laden (greift auf .env datei zurück)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///familiendashboard.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL', 'sqlite:///familiendashboard.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     # Extensions mit app verbinden
     db.init_app(app)
-    
+
     # Blueprints registrieren
     from app.routes import main_bp, example_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(example_bp)
-    
+
     return app
